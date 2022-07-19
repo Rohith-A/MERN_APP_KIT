98445c4f62346d3a124d6dbeafba800b61cc9fef
Rohith-A <rohithchintu1914@gmail.com>
2022-07-19 11:00:44 +0530

    Updates

---
+++ precache-manifest.152e6d1e13b24e781b3eeea42556c2a1.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f84fb7ccf73250a9818ce3f9e520bb3",
+    "url": "/MERN_APP_KIT/index.html"
+  },
+  {
+    "revision": "2c9bbcda2f6b49f13e0a",
+    "url": "/MERN_APP_KIT/static/css/2.c3bb4719.chunk.css"
+  },
+  {
+    "revision": "8e38ebfc2980e742198e",
+    "url": "/MERN_APP_KIT/static/css/main.c1fb002d.chunk.css"
+  },
+  {
+    "revision": "2c9bbcda2f6b49f13e0a",
+    "url": "/MERN_APP_KIT/static/js/2.888ea593.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MERN_APP_KIT/static/js/2.888ea593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e38ebfc2980e742198e",
+    "url": "/MERN_APP_KIT/static/js/main.51739aa5.chunk.js"
+  },
+  {
+    "revision": "52ac6a49597d03782527",
+    "url": "/MERN_APP_KIT/static/js/runtime-main.39e919b2.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/MERN_APP_KIT/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "ace353ab88a5d752f97cf1104cb10487",
+    "url": "/MERN_APP_KIT/static/media/covid19_response_icon.ace353ab.svg"
+  },
+  {
+    "revision": "cc9a2475e7457ee818d4b2d71e7ab022",
+    "url": "/MERN_APP_KIT/static/media/delete.cc9a2475.png"
+  },
+  {
+    "revision": "1eec8b6704916c0ff825801e9e0b5bfb",
+    "url": "/MERN_APP_KIT/static/media/edit.1eec8b67.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/MERN_APP_KIT/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/MERN_APP_KIT/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "2d2afb2719a1ee903e576e7c457daf81",
+    "url": "/MERN_APP_KIT/static/media/primeicons.2d2afb27.eot"
+  },
+  {
+    "revision": "66ee0deb739ca71f0ecdc39d7c1b22cb",
+    "url": "/MERN_APP_KIT/static/media/primeicons.66ee0deb.woff"
+  },
+  {
+    "revision": "df0140f8e79ecfeffaf85220aaecd7c4",
+    "url": "/MERN_APP_KIT/static/media/primeicons.df0140f8.ttf"
+  },
+  {
+    "revision": "e5e0e94474d5fd92e7e800a8865d297c",
+    "url": "/MERN_APP_KIT/static/media/primeicons.e5e0e944.svg"
+  }
+]);
